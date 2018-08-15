5115544fc2a4a3ca6b5c0d9ff9ea3243608214d6
ductoanthanh <toanth@metropolia.fi>
2018-08-15 19:14:41 +0300

    Update tests & config separate test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyCK-itMOsYWHXmQM6hAmSUbCVZQfcZwZ9s",
-    authDomain: "expense-tracker-ac86c.firebaseapp.com",
-    databaseURL: "https://expense-tracker-ac86c.firebaseio.com",
-    projectId: "expense-tracker-ac86c",
-    storageBucket: "expense-tracker-ac86c.appspot.com",
-    messagingSenderId: "218474150336"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
 
 firebase.initializeApp(config);
